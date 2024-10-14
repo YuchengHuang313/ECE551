a52158b4a3e757804303aaebde2fc9830a7390dc
yhuang546 <yhuang546@ad.engr.wisc.edu>
2024-10-14 16:01:25 -0500

    TaskCompleted - HW3 is completed

--- HW3/Q5/Dterm_tb.sv
+++ HW3/Q5/Dterm_tb.sv
@@ -20,7 +20,7 @@ initial begin
 	rst_n = 1;
 	err_vld = 1;
 	repeat(3) @(negedge clk); // wait for 3 cycles till data arrives 
-	if (D_term == 13'd0)
+	if (D_term !== 13'd0)
 		$display("err_sat did not change in the last 3 cycle, D_term should be 0, but it is %x", D_term);
 	else 
 		$display("Test Case 1 Passed");
@@ -29,7 +29,40 @@ initial begin
 	@(negedge clk);
 	err_sat = 10'h00a;
 	@(negedge clk);
+	if (D_term !== 13'h1fdd)
+		$display("D_term should be 1fdd but it is %h", D_term);
+	else 
+		$display("Test Case 2 Passed");
+		
+	// Test Case 3: check if D_term goes back to 0 after 3 cycles
+	repeat(3) @(negedge clk);
+	if (D_term !== 13'd0)
+		$display("err_sat did not change in the last 3 cycle, D_term should be 0, but it is %x", D_term);
+	else 
+		$display("Test Case 3 Passed");
 	
+	// Test Case 4: check if err_vld works
+	@(negedge clk);
+	err_vld = 0;
+	err_sat = 10'h00b;
+	repeat(10) @(negedge clk); // D_term should be constant over this period
+	if (D_term !== 13'h007)
+		$display("D_term should be %x but it is %x", 13'h007, D_term);
+	else 
+		$display("Test Case 4 passed");
+	
+	// Test Case 5: after releasing err_vld, next cycle should calculate based on data before err_vld = 1;
+	@(negedge clk);
+	err_vld = 1;
+	err_sat = 10'h00c;
+	@(negedge clk);
+	if (D_term !== 13'h00e)
+		$display("D_term should be 13'h00e, but it is %x", D_term);
+	else 
+		$display("Test Case 5 passed");
+		
+	
+		
 	repeat(10) @(negedge clk);
 	$stop(); 
 
