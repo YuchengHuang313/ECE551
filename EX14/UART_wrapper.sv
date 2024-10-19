e52238aec2d25e4abf4f28c4d9768165d1808e9d
Yucheng Huang <yhuang546@wisc.edu>
2024-10-19 00:12:00 -0500

    FileUpload - added files for Ex14 and Ex15

--- EX14/UART_wrapper.sv
+++ EX14/UART_wrapper.sv
@@ -1,7 +1,7 @@
 module UART_wrapper (input clr_cmd_rdy, input RX, input trmt, input [7:0] resp, 
-						output cmd_rdy, output [15:0] cmd, output tx_done, output TX);
+						output logic cmd_rdy, output logic [15:0] cmd, output tx_done, output TX);
 
-logic rx_rdy, clr_rdy, trmt, tx_done;
+logic rx_rdy, clr_rdy;
 logic [7:0] rx_data, tx_data;
 
 UART uart(.rx_rdy(rx_rdy), .rx_data(rx_data), .clr_rdy(clr_rdy), .trmt(trmt), 
@@ -15,9 +15,9 @@ logic [7:0] high_byte;
 logic byte_sel, set_cmd_rdy;
 
 // dataflow within the wrapper
-always_ff @(posedge clk, negedge rst_n);
+always_ff @(posedge clk, negedge rst_n)
 	if (!rst_n)
-		high_byte <= 2'h00;
+		high_byte <= 2'h0;
 	else if (byte_sel)
 		high_byte <= rx_data;
 assign cmd = {high_byte, rx_data};
@@ -28,35 +28,36 @@ always_ff @(posedge clk, negedge rst_n)
 		cmd_rdy <= 1'b0;
 	else if (set_cmd_rdy)
 		cmd_rdy <= 1'b1;
-	else if (clr_cmd_rdy |
+	else if (clr_cmd_rdy | byte_sel)
+		cmd_rdy <= 1'b0;
 
 // FSM flop
-always_ff @(posedge clk, negedge rst_n);
+always_ff @(posedge clk, negedge rst_n)
 	if (!rst_n)
 		state <= HIGH;
 	else 
 		state <= nxt_state;
 		
 always_comb begin
-	byte_sel = 1'b1;
-	clr_cmd_rdy = 1'b0;
+	byte_sel = 1'b0;
 	clr_rdy = 1'b0;
-	set_cmd_rdy = 0;
-	
+	set_cmd_rdy = 1'b0;
+	nxt_state = state;
 	case(state)
 		default: // HIGH state
 			if (rx_rdy) begin
-				byte_sel = 1'b0;
-				clr_cmd_rdy = 1'b1;
+				byte_sel = 1'b1;
 				clr_rdy = 1'b1;
+				nxt_state = LOW;
 			end
 			
 		LOW: 
 			if (rx_rdy) begin
 				set_cmd_rdy = 1'b1;
 				clr_rdy = 1'b1;
+				nxt_state = HIGH;
 			end 
 	endcase
 end
 
-endmodule;						
+endmodule		
